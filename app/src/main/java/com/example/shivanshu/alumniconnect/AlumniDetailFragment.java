14f27f7a53d89a8aeaa1823a1b325b7d29f58c7a
Digvijay <digvijay.14bcs2008@abes.ac.in>
2017-02-17 03:34:27 -0800

    Validation Complete

--- app/src/main/java/com/example/shivanshu/alumniconnect/AlumniDetailFragment.java
+++ app/src/main/java/com/example/shivanshu/alumniconnect/AlumniDetailFragment.java
@@ -1,12 +1,20 @@
 package com.example.shivanshu.alumniconnect;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static com.example.shivanshu.alumniconnect.R.drawable.selected_box;
 
 
 /**
@@ -17,16 +25,13 @@ import android.view.ViewGroup;
  * Use the {@link AlumniDetailFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AlumniDetailFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class AlumniDetailFragment extends Fragment implements View.OnClickListener{
+   int[]  ValidationId={R.id.name_validation,R.id.college_validation,R.id.company_validation,R.id.email_validation,R.id.phone_number_validation,R.id.tech_validation};
+   private static TextView[] ValidationViews=new TextView[7];
+    int[] EditTextId={R.id.name,R.id.college_name,R.id.company_name,R.id.email_id,R.id.contact_number,R.id.working_technology};
+    static EditText[] AlumniDetails=new EditText[7];
+    int i=0;
+int count=0;
     private OnFragmentInteractionListener mListener;
 
     public AlumniDetailFragment() {
@@ -55,19 +60,26 @@ public class AlumniDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            //mParam1 = getArguments().getString(ARG_PARAM1);
+            //mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alumni_detail, container, false);
+        View view=inflater.inflate(R.layout.fragment_alumni_detail, container, false);
+        for(i=0;i<ValidationId.length;i++)
+        {Log.d("digvijay","clicked");
+            ValidationViews[i]=(TextView)view.findViewById(ValidationId[i]);
+            AlumniDetails[i]=(EditText)view.findViewById(EditTextId[i]);
+            String s=AlumniDetails[i].getText().toString();
+            Log.d("digvijay","THIS IS DIGVIJAY");
+            AlumniDetails[i].setOnClickListener(this);
+        }
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onAlumniDetailInteraction();
@@ -89,6 +101,15 @@ public class AlumniDetailFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        for( i=0;i<AlumniDetails.length;i++)
+        {
+            ValidationViews[i].setVisibility(View.VISIBLE);
+        }
+
     }
 
     /**
